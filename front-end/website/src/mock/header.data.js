7607296166ea72f8d13b5bcba1e7162e87e18ee6
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2024-04-02 16:32:34 +0700

    fix: tailwind

---
+++ front-end/website/src/mock/header.data.js
@@ -0,0 +1,59 @@
+import {
+    Bars4Icon,
+    GlobeAmericasIcon,
+    NewspaperIcon,
+    PhoneIcon,
+    RectangleGroupIcon,
+    SquaresPlusIcon,
+    SunIcon,
+    TagIcon,
+    UserGroupIcon,
+} from "@heroicons/react/24/solid";
+
+const navListMenuItems = [
+    {
+        title: "Products",
+        description: "Find the perfect solution for your needs.",
+        icon: SquaresPlusIcon,
+    },
+    {
+        title: "About Us",
+        description: "Meet and learn about our dedication",
+        icon: UserGroupIcon,
+    },
+    {
+        title: "Blog",
+        description: "Find the perfect solution for your needs.",
+        icon: Bars4Icon,
+    },
+    {
+        title: "Services",
+        description: "Learn how we can help you achieve your goals.",
+        icon: SunIcon,
+    },
+    {
+        title: "Support",
+        description: "Reach out to us for assistance or inquiries",
+        icon: GlobeAmericasIcon,
+    },
+    {
+        title: "Contact",
+        description: "Find the perfect solution for your needs.",
+        icon: PhoneIcon,
+    },
+    {
+        title: "News",
+        description: "Read insightful articles, tips, and expert opinions.",
+        icon: NewspaperIcon,
+    },
+    {
+        title: "Products",
+        description: "Find the perfect solution for your needs.",
+        icon: RectangleGroupIcon,
+    },
+    {
+        title: "Special Offers",
+        description: "Explore limited-time deals and bundles",
+        icon: TagIcon,
+    },
+];
