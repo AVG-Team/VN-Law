69f6bb87a69c5579acbdaaf7b6f91e9429e62f18
Nguyễn Tấn Dũng <ntdvlog1683@gmail.com>
2024-04-24 07:10:14 +0700

    merge main

---
+++ auth-service/src/main/java/fit/hutech/service/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package fit.hutech.service.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
