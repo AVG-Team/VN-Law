8a0990c415677a64e00289ec975d4d21798659a1
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2024-09-25 02:00:01 +0700

    feat: add mapper

--- law-service/src/main/java/avg/vnlaw/lawservice/dto/request/FilesRequest.java
+++ law-service/src/main/java/avg/vnlaw/lawservice/dto/request/FilesRequest.java
@@ -10,7 +10,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class FileRequest {
+public class FilesRequest {
     Integer id;
     String link;
     String path;
