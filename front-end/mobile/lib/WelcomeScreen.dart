150f1f16a9623a425efeb2292f4ac312e32def8a
Asa Nguyen <96124608+Asa-Nguyen@users.noreply.github.com>
2024-10-11 12:50:28 +0700

    feat: UI Login Register

---
+++ front-end/mobile/lib/WelcomeScreen.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/regScreen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'loginScreen.dart';
+import 'dart:async'; // For Timer
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xffFFFFFF),
+              Color(0xffD6EAF8),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 200.0),
+              child: Image(image: AssetImage('assets/logo.png')),
+            ),
+            const SizedBox(height: 100),
+            const TypewriterText(text: 'Tri Thức Pháp Luật Việt Nam'), // Use the typewriter text widget
+            const SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+              },
+              child: Container(
+                height: 55,
+                width: 320,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: const Color.fromARGB(255, 116, 192, 252),
+                ),
+                child: const Center(
+                  child: Text(
+                    'SIGN IN',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegScreen()),
+                );
+              },
+              child: Container(
+                height: 53,
+                width: 320,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.white),
+                ),
+                child: const Center(
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(),
+            const Text(
+              'Login with Social Media',
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 12),
+            Transform.translate(
+              offset: const Offset(0, -10),
+              child: SvgPicture.asset(
+                'assets/social.svg',
+                height: 30,
+                width: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TypewriterText extends StatefulWidget {
+  final String text;
+
+  const TypewriterText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _TypewriterTextState createState() => _TypewriterTextState();
+}
+
+class _TypewriterTextState extends State<TypewriterText> {
+  String _displayedText = '';
+  int _index = 0;
+  late Timer _timer; // Declare the Timer
+
+  @override
+  void initState() {
+    super.initState();
+    _startTyping();
+  }
+
+  void _startTyping() {
+    _timer = Timer.periodic(const Duration(milliseconds: 150), (timer) {
+      if (_index < widget.text.length) {
+        setState(() {
+          _displayedText += widget.text[_index];
+          _index++;
+        });
+      } else {
+        _index = 0; // Reset index to start from the beginning
+        _displayedText = ''; // Clear displayed text
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel(); // Cancel the timer when the widget is disposed
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _displayedText,
+      style: const TextStyle(
+        fontSize: 30,
+        color: Colors.black,
+      ),
+    );
+  }
+}
